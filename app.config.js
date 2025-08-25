37175a12d76a68aaf1982c2d131eaf549e054b80
Dionysios <dionysios8292@gmail.com>
2025-08-25 09:06:59 +0100

    update

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "WorldWise Trivia App",
     slug: "WorldWiseTriviaApp",
-    version: "1.0.8",
+    version: "1.0.9",
     newArchEnabled: true,
     orientation: "portrait",
     icon: "./assets/logo2.png",
